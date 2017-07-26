a77e4757d6a2ae0cd1b4337e71992449d039425a
Youss06 <oumar.youssouf0605@gmail.com>
2017-07-26 15:07:58 +0200

    finish first ex

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var user= {
+  firstName: "Paul",
+  lastName: "Gray",
+  age: "43",
+  année: "2017",
+  payment: ["visa", "mastercard"],
+}
+alert(user.firstName);
+alert(user.lastName);
+alert(user.age);
+alert(user.année-user.age);
