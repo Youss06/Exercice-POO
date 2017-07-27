16f53b70991818f52477a23d11b2172811821f87
Youss06 <oumar.youssouf0605@gmail.com>
2017-07-27 09:20:14 +0200

    add contry var

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 var user= {
   firstName: "Paul",
   lastName: "Gray",
+  contry: "France",
   age: "43",
   année: "2017",
   payment: ["visa", "mastercard"],
 }
 alert(user.firstName);
 alert(user.lastName);
+alert(user.contry);
 alert(user.age);
 alert(user.année-user.age);
