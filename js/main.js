3dee217018606c1431738c2007f2abd76a505172
Youss06 <oumar.youssouf0605@gmail.com>
2017-08-29 13:34:50 +0200

    add function to clear input

--- js/main.js
+++ js/main.js
@@ -69,9 +69,8 @@ function guerrier(nom, attack, défense, santé){
   this.santé= santé;
 
   this.fightGuerrier = function(X){
-    this.santé-=X.attack;
-    console.log(fighter2.santé);
-
+    X.santé-=this.attack;
+    console.log(X.santé);
   };
 }
 
@@ -82,22 +81,53 @@ fighter1.fightGuerrier(fighter2);
 fighter2.fightGuerrier(fighter1);
 
 
-function guerrier(nom, attack, défense, santé, mana){
+function magicien(nom, attack, défense, santé, mana){
   this.nom= nom;
   this.attack= attack;
   this.défense= défense;
   this.santé= santé;
   this.mana=mana;
+
+  this.soin = function(soigner) {
+    if (mana>10) {
+      this.mana-=10;
+      this.santé+=10;
+    }
+  };
+  this.fightGuerrier = function(X){
+    X.santé-=this.attack;
+    console.log(X.mana);
+
+  };
 }
 
+var merlin = new magicien ("pop", 12, 7, 100, 60);
+
+fighter1.fightGuerrier(merlin);
+fighter2.fightGuerrier(merlin);
+magicien.soin();
 
 
+function infoProduit (titre , phrase , description){
 
+    this.titre = titre;
+    this.phrase = phrase;
+    this.description = description;
 
+  }
 
+function formulaire() {
+    var a = document.getElementById("myTitle").value;
+    document.getElementById("titre").innerHTML = a;
 
-function formualire() {
-    var x = document.getElementById("myTitle").value;
-    document.getElementById("titre").innerHTML = x;
+    var b = document.getElementById("phscatch").value;
+    document.getElementById("phrase").innerHTML = b;
+
+    var c = document.getElementById("description").value;
+    document.getElementById("descrip").innerHTML = c;
+
+// var d = " ";
+    document.getElementById("myTitle").value = " ";
+    document.getElementById("phscatch").value = " ";
+    document.getElementById("description").value = " ";
 }
-formualire()
